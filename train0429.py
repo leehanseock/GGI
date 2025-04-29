a7cf0d6938628204286f7936b6b80de8df54750e
leehanseock <leehanseock@gmail.com>
2025-04-29 11:00:14 +0900

    batchsize 32->8

--- train0429.py
+++ train0429.py
@@ -1,14 +1,15 @@
 from ultralytics import YOLO
 
-# 모델 불러오기
-model = YOLO('D:/GGI/yolo/yolov12n.pt')
+if __name__ == "__main__":
+    # 모델 불러오기
+    model = YOLO('D:/GGI/yolo/yolov12n.pt')
 
-# 학습 시작
-model.train(
-    data='D:/GGI/datasets/Smoking-CCTV-Detection.v1i.yolov12/data.yaml',
-    epochs=100,
-    imgsz=640,
-    batch=32,
-    device=0,
-    verbose=True  # << 여기
-)
+    # 학습 시작
+    model.train(
+        data='D:/GGI/datasets/Smoking-CCTV-Detection.v1i.yolov12/data.yaml',
+        epochs=100,
+        imgsz=640,
+        batch=8,
+        device=0,
+        verbose=True  # 이거까지 추가했지
+    )
