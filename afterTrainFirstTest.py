b073e059e1c89402da078ea66c064fa9c4c899a9
leehanseock <leehanseock@gmail.com>
2025-04-30 13:45:38 +0900

    모델 테스트 with or without camera

--- afterTrainFirstTest.py
+++ afterTrainFirstTest.py
@@ -1,22 +1,26 @@
 from ultralytics import YOLO
 import cv2
 
-# 학습된 모델 로드
-model = YOLO("runs/detect/train7/weights/best.pt")
+# 모델 로드
+model = YOLO("runs/detect/train11/weights/best.pt")
 
-# 테스트할 이미지 경로
-image_path = "D:/GGI/test_images/2.jpg"  # ← 원하는 이미지 경로로 수정하세요
-
-# 이미지 읽기
+# 테스트 이미지 경로
+image_path = "D:/GGI/test_images/2.jpg"
 image = cv2.imread(image_path)
 
-# 추론 실행
-results = model(image)
+# 추론
+results = model.predict(source=image, conf=0.1, iou=0.4, show=True)
+
+# ✅ 여기서 박스 결과를 출력!
+for box in results[0].boxes:
+    cls_id = int(box.cls)
+    conf = float(box.conf)
+    print(f"Class: {model.names[cls_id]}, Conf: {conf:.3f}")
 
 # 결과 시각화
 annotated = results[0].plot()
 
-# 결과 출력
+# 화면에 결과 표시
 cv2.imshow("YOLO Smoking Detection - Image Test", annotated)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
